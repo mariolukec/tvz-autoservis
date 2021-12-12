491e70e22e1d429af186186b2b7e4df936ccfe0b
mariolukec <mario.lukec@ext.reev.com>
2021-12-12 21:05:41 +0100

    fix jar build

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ tasks.withType<Jar> {
         attributes["Main-Class"] = "tvz.lukec.autoservice.AutoserviceApplicationKt"
     }
 
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    duplicatesStrategy = DuplicatesStrategy.INHERIT
 
     // To add all of the dependencies
     from(sourceSets.main.get().output)
