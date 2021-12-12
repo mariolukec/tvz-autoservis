a21d7f7fc0b4358373a1cded03e068886277d26a
mariolukec <mario.lukec@ext.reev.com>
2021-12-12 23:00:53 +0100

    update build gradle

--- src/main/kotlin/tvz/lukec/autoservice/AutoserviceApplication.kt
+++ src/main/kotlin/tvz/lukec/autoservice/AutoserviceApplication.kt
@@ -5,11 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import tvz.lukec.autoservice.rest.config.security.OAuth2ResourceServerConfig
 
-@SpringBootApplication(
-        exclude = [
-            OAuth2ResourceServerConfig::class
-        ]
-)
+@SpringBootApplication
 class AutoserviceApplication
 
 fun main(args: Array<String>) {
