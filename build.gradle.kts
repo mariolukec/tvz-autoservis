66ae4722c76bfa3087721bd8a997fff21232f496
mariolukec <mario.lukec@ext.reev.com>
2021-12-12 22:05:21 +0100

    update build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,15 +31,13 @@ tasks.withType<Jar> {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
 //    from(configurations.compileClasspath.get().map { if (it.isDirectory()) it else zipTree(it) })
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory()) it else zipTree(it) })
 
     // To add all of the dependencies
-//    from(sourceSets.main.get().output)
-//
-//    dependsOn(configurations.runtimeClasspath)
-//    from({
-//        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-//    })
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory()) it else zipTree(it) })
+
 }
 
 dependencies {
